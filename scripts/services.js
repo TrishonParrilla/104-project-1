8645876e37049a71d5bb7d1ad7936c333aa68984
TrishonParrilla <trishonp1@gmail.com>
2026-02-19 07:57:14 -0700

    Implement dark mode feature and update stylesheets; add font preconnects and improve form validation

--- scripts/services.js
+++ scripts/services.js
@@ -6,4 +6,42 @@ $("#servicesForm").submit(function(event) {
     const name = $("#serviceName").val().trim();
     const description = $("#serviceDescription").val().trim();
     const price = $("#servicePrice").val();
+
+    //if (name == "" || description == "" || price <= 0 ){
+        // alert("All fields required");
+       // $("#serviceName").css("border", "solid, 2px, red");
+    //}
+
+    if(!name){
+         $("#serviceName").css("border", "solid, 2px, red");
+
+    }
+    else { 
+        $("#serviceName").css("border","");
+
+    }
+    if(!description){
+         $("#serviceDescription").css("border", "solid, 2px, red");
+
+    }
+    else { 
+        $("#serviceDescription").css("border","");
+
+    }
+    if(!price){
+         $("#servicePrice").css("border", "solid, 2px, red");
+
+    }
+    else { 
+        $("#servicePrice").css("border","");
+
+    }
+    this.reset();
+});
+
+
+//darkmode
+
+$("#changeModeButton").click(function(){
+    $("body").toggleClass("dark-mode");
 });
